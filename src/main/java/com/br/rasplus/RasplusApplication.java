029b6d5fe31b0cf29e387408e00a5d47cb25bbc4
Julio Moraes <juliocesardemoraes2015@gmail.com>
2023-03-12 18:42:40 -0300

    estrutura inicial

---
+++ src/main/java/com/br/rasplus/RasplusApplication.java
@@ -0,0 +1,13 @@
+package com.br.rasplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RasplusApplication.class, args);
+    }
+
+}
